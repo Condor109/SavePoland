778c5aa33184623446211f1c28cf2827d67d7773
Condor <124301262+Condor109@users.noreply.github.com>
2023-02-20 16:09:57 -0500

    Create letter by letter animation and add style to the page

--- random.js
+++ random.js
@@ -1,30 +1,32 @@
-function NPCchoice(){
-  let y=Math.round(Math.random()*3)
-  switch (y){
-    case 0: return 0
-    case 1: return 1
-    case 2: return 2
+function letter_move(id,message){
+  let doc=document.getElementById(id);
+  let i=0;
+  let int=setInterval(frame,100);
+  function frame(){
+    if (j==3){
+      return;
+    }
+    else if (i==message.length){
+      clearInterval(int);
+      j++;
+      letter_move(id_p[j],messages[j])
+    }
+    
+    else{
+      let text_node=document.createTextNode(message[i]);
+      doc.appendChild(text_node);
+      i++;
+    }
   }
 }
-function getChoice(){
-  let y=prompt("INFANTRY CAVALRY ARMORED");
-  y=y.toUpperCase();
-
-  console.log(y);
-  return y
+function slide(id){
+  let doc=document.getElementById(id);
+  
 }
 
-function Compare(n,y){
-  if (y[n[1]]==n[0]){
-    console.log("IT IS A TIE")
-  }
-  else if(n[0]==y[n[1]-1] ){
-    console.log("YOU WIN")
-  }
-  else{
-    console.log("YOU LOSE")
-  }
-}
-
-var n=[getChoice(),NPCchoice()];
-Compare(n,["INFANTRY","CAVALRY","ARMORED"])
+var j=0;
+id_p=["p_1","p_2","p_3"];
+messages=["1 SEPTEMBER 1939",
+"Germany has started the invasion of Poland. As the major of its ground forces,you will have to take the right (and random) decisions to defend the Polish Fatherland."
+, "THE DESTINY OF POLAND IS IN YOUR HANDS"]
+letter_move(id_p[j],messages[j])
