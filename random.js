953c6c90309174d724046d5d9a1d83ee1811edea
Condor <124301262+Condor109@users.noreply.github.com>
2023-02-21 23:31:12 -0500

    Add opacity and height to div with 3 imgs
    In a start it was used a function that creates a div from zero, but as
    the code was long and over-complicated, it was decided that the
    DOM will only hide the div and show it frame per frame changing his opacity.

--- random.js
+++ random.js
@@ -4,6 +4,7 @@ function letter_move(id,message){
   let int=setInterval(frame,100);
   function frame(){
     if (j==3){
+      change()
       return;
     }
     else if (i==message.length){
@@ -19,30 +20,45 @@ function letter_move(id,message){
     }
   }
 }
-function slide(id, id_p){
+/*function add_img(id,new_id,n){
   let doc=document.getElementById(id);
-  let doc_1=document.getElementById(id_p);
-  let rect_1=doc_1.getBoundingClientRect();
-  let rect=doc.getBoundingClientRect();
-  let i=-250;
-  let int=setInterval(frame,
-    1);
+  let dv=document.createElement("div");
+  let img=null;
+  let i=1;
+  let int=setInterval(frame,50);
   function frame(){
-    if(rect.bottom<=rect_1.bottom){
-      clearInterval(int);
+    if(i<=0){
+      clearInterval(int)
     }
     else{
-      rect=doc.getBoundingClientRect();
-      doc.style.bottom=i+"px"
-      i+=0.1;
+      doc.style.opacity=i.toString();
+      i-=0.1;
     }
   }
+  for(let i=0;i<n;i++){
+    let img=document.createElement("img");
+    img.src=imgs[i];
+    console.log(imgs[0])
+    dv.appendChild(img);
+  }
+  dv.setAttribute('id','d_1')
+  document.body.insertBefore(dv,document.getElementById("p_2"))
+}*/
+//add_img("p_1","d_1",3)
+
+function change(){
+  doc=document.querySelector("#d_1");
+  console.log(doc.style.height)
+  doc.style.height="auto"
+  doc.style.opacity="1"
+
 }
 
 var j=0;
+var imgs=["./img/soldier.png","./img/cavalry.png","./img/tank.png"]
 id_p=["p_1","p_2","p_3"];
-messages=["1 SEPTEMBER 1939",
+messages=["THE DESTINY OF POLAND IS IN YOUR HANDS","1 SEPTEMBER 1939",
 "Germany has started the invasion of Poland. As the major of its ground forces,you will have to take the right (and random) decisions to defend the Polish Fatherland."
-, "THE DESTINY OF POLAND IS IN YOUR HANDS"]
+]
 letter_move(id_p[j],messages[j]);
-slide("d_1","p_3");
+change();
